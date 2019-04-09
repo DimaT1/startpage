1945e023b3c5a479c77f4b3545bfc96f207b56b0
DimaT1 <dymych99@gmail.com>
2019-04-10 00:16:26 +0300

    deleted anime

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function randomize_background(){
-    var backgrounds = ["url('back1.png')",
-                       "url('back2.jpg')",];
+    var backgrounds = ["url('back1.jpg')",
+                       "url('back2.jpg')",
+                       "url('back3.jpg')",
+                       "url('back4.png')",
+                       "url('back5.jpg')",
+                       "url('back7.jpg')",
+                       "url('back8.jpg')",
+                       "url('back9.jpg')",
+                       "url('back10.jpg')",
+    ];
 
     var random = Math.floor(Math.random() * backgrounds.length);
     document.body.style.background = backgrounds[random];
